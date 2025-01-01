3ab3921467bff8d8742230e226d21beda700521d
hacaric <jan@iskra.sk>
2025-01-01 13:49:05 +0100

    Updated info in check_requiements

--- check_requiements.py
+++ check_requiements.py
@@ -1,5 +1,6 @@
 import sys
 import os
+print("Checking requirements...")
 python_version = float(f"{sys.version_info.major}.{sys.version_info.minor}")
 
  
@@ -16,4 +17,4 @@ finally:
 print("Python version:", python_version, "-->", "required version is 3.10." if python_version < 3.10 else "version high enough.")
 print("Pygame not installed." if not pygame_installed else "Pygame installed.")
 
-print("\nOverview:\n", "All packages are installed. For game start run 'main.py' file." if pygame_installed and python_version >= 3.10 else (("Install python version 3.10 or higher." if python_version < 3.10 else "") + ("Install pygame: $pip install pygame" if not pygame_installed else "")))
+print("\nOverview:\n", "All packages are installed. For game start run 'python ./launcher/launcher.py'." if pygame_installed and python_version >= 3.10 else (("Install python version 3.10 or higher." if python_version < 3.10 else "") + ("Install pygame: $pip install pygame" if not pygame_installed else "")))
